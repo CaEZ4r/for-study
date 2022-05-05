612e5ee40a7f028eee9dfd553a20e0405fa4ebdc
NikitaDzer <nikita.dzer.g@gmail.com>
2022-05-05 13:13:33 +0300

    grph

--- GET/graphic.py
+++ GET/graphic.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open("settings.txt", "r") as data:
+with open("settings.txt", "r") as data: 
     tmp = [float(i) for i in data.read().split("\n")]
 
 data_array = np.loadtxt("data.txt", dtype = int)
@@ -9,6 +9,7 @@ data_array = np.loadtxt("data.txt", dtype = int)
 fig, ax = plt.subplots(figsize = (16, 10), dpi=400) #настройка картинки
 
 x = np.arange(0, len(data_array))
+
 ax.set_xlim(0, 10) #оси
 ax.set_ylim(0, 3.5)
 ax.set_xlabel("Время, с", fontsize=10)
@@ -19,18 +20,22 @@ ax.set_title("Зависимость напряжения на RC-цепи от 
 xi = np.arange(0, max(x), 20)
 yi = np.interp(xi, x, data_array)
 
-ax.plot(x * tmp[1], data_array * tmp[0], '-', color = 'green', label='V(t)') #график
-ax.plot( xi * tmp[1], yi * tmp[0], marker='o', ls='None', color='g')
+markers=np.arange(0, max(x), 20)
+
+ax.plot(x * tmp[1], data_array * tmp[0], '-o', markevery=markers, color = 'green', label='V(t)') #график
+
 
 ax.legend(loc='best', fontsize='x-large') #легенда
-ax.xaxis.set_minor_locator(plt.MultipleLocator(0.2))
+
+ax.xaxis.set_minor_locator(plt.MultipleLocator(0.2)) #ticks
 ax.yaxis.set_minor_locator(plt.MultipleLocator(0.1))
 plt.tick_params(axis='both', which='major', direction='inout', length=10)
 plt.tick_params(axis='both', which='minor', direction='inout', length=6)
-ax.grid(which='major', linestyle='-')
+
+ax.grid(which='major', linestyle='-') #сетка
 ax.grid(which='minor', linestyle='--')
 
-ax.text(8.0, 3.1, 'Время заряда = {} c'.format(round(np.argmax(data_array) * tmp[1], 2)), fontsize='x-large')
+ax.text(8.0, 3.1, 'Время заряда = {} c'.format(round(np.argmax(data_array) * tmp[1], 2)), fontsize='x-large') #текст на графике
 ax.text(8.0, 2.9, 'Время разряда = {} c'.format(round(len(data_array) * tmp[1] - np.argmax(data_array) * tmp[1], 2)), fontsize='x-large')
 
-fig.savefig("test.svg")
+fig.savefig("test.svg") #сохранение
